59ce8f3f3caa940185a7b416dcae7611c4176c6f
Samkim-96 <160379496+Samkim-96@users.noreply.github.com>
2024-10-16 21:56:30 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+//initialize dependences
+
+const express= require("express");
+const app= express();
+const mysql= require('mysql2');
+const dotenv= require('dotenv');
+const cors= require('cors');
+const { Server } = require("net");
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+//connect to the database
+
+const db= mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+//Check if db connection works
+db.connect((err)=>{
+    //no wedding today
+    if(err) return console.log("error connecting to the mysql db");
+    //yes connected
+    console.log("connected to mysql successfully as id:", db.threadId)
+
+// Your code goes here
+// Get Method Example
+app.set('view engine','ejs');
+app.set('views', __dirname + '/views');
+
+// Data is the name of file for inside views folder
+app.get('/data', (req,res) =>{
+    // Retrieve data from database
+    db.query('SELECT *FROM patients',(err, results) =>{
+        if (err){
+            console.error(err);
+            res.status(500).send('Error retrieving data');
+        }else{
+            // Display the records to the browser
+            res.render('data', {results: results});
+        }
+    });
+});
+
+    app.listen(process.env.PORT, () =>{
+console.log(`Server listening on port ${process.env.PORT}`);
+//Send a message to the browser
+console.log('Sending message to the browser..');
+app.get('/', (req,res) =>{
+    res.send('server started successfully! wedding can go On!!')
+});
+    });
+});
